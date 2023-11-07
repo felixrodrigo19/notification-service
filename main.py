7a9d34425cfe1eade7dac6855857a5e7c0c401cc
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2023-11-06 22:52:09 -0300

    feat: create celery objec

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os
 
 from config import settings
+from tasks.task_manager import TaskManager
 
 if __name__ == "__main__":
     os.environ['ENV'] = "default"
     settings.setenv(os.getenv('ENV'))
+
+    task_manager = TaskManager()
+    task_manager.setup_task_manager()
